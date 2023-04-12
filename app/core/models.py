eaab3dc2b1ef8f060203757f7b5a751009f76bef
an95ew <an95ew@gmaill.com>
2023-04-12 16:28:32 +0200

    User and Token

--- app/core/models.py
+++ app/core/models.py
@@ -15,6 +15,7 @@ class UserManager(BaseUserManager):
 
     def create_user(self, email, password=None, **extra_fields):
         """Create, save and return new user"""
+
         if not email:
             raise ValueError('User must have an Email address')
         user = self.model(email=self.normalize_email(email), **extra_fields)
@@ -25,6 +26,7 @@ class UserManager(BaseUserManager):
 
     def create_superuser(self, email, password):
         """Create and return new superuser"""
+
         user = self.create_user(email, password)
         user.is_staff = True
         user.is_superuser = True
