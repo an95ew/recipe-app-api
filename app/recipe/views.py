538281064b60e10d843f48e0b5e793f7c84893a0
an95ew <an95ew@gmaill.com>
2023-05-26 19:14:48 +0300

    14. Tags API Section Update

--- app/recipe/views.py
+++ app/recipe/views.py
@@ -39,7 +39,10 @@ class RecipeViewSet(viewsets.ModelViewSet):
         serializer.save(user=self.request.user)
 
 
-class TagViewSet(mixins.ListModelMixin, viewsets.GenericViewSet):
+class TagViewSet(mixins.DestroyModelMixin,
+                 mixins.UpdateModelMixin,
+                 mixins.ListModelMixin,
+                 viewsets.GenericViewSet):
     """Manage tags in the database."""
     serializer_class = serializers.TagSerializer
     queryset = Tag.objects.all()
@@ -49,10 +52,3 @@ class TagViewSet(mixins.ListModelMixin, viewsets.GenericViewSet):
     def get_queryset(self):
         """Retrieve recipes for authenticated user."""
         return self.queryset.filter(user=self.request.user).order_by('-name')
-
-
-
-
-
-
-
