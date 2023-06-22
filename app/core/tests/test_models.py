361361be7703a14e56f32f27651b12f9ac0cccb9
an95ew <an95ew@gmaill.com>
2023-06-22 18:06:44 +0300

    Last update

--- app/core/tests/test_models.py
+++ app/core/tests/test_models.py
@@ -78,7 +78,15 @@ class ModelTest(TestCase):
 
         self.assertEqual(str(tag), tag.name)
 
+    def test_create_ingredient(self):
+        """Test creating an ingredient is successful."""
+        user = create_user()
+        ingredient = models.Ingredient.objects.create(
+            user=user,
+            name='Ingredient'
+        )
 
+        self.assertEqual(str(ingredient), ingredient.name)
 
 
 
